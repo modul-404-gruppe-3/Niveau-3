ce585ec5a6fa1d2b95db9273aaa5ddfe13c30852
LucaGuettinger <luca.guettinger@cmiag.ch>
2019-04-17 11:48:15 +0200

    remove useless step when entering product list

--- src/main/java/me/niveau3/services/mark/ProductService.java
+++ src/main/java/me/niveau3/services/mark/ProductService.java
@@ -24,42 +24,27 @@ public class ProductService extends AbstractProgram implements IStopable {
         for (Product product : mainService.getProductManager().getProducts().values()) {
             System.out.println("[" + product.getId()+ "] " + product.getName() + ", " + product.getPrice() + " CHF");
         }
+        
+        System.out.println("Geben sie die Id des Objektes an das sie zum Warenkorb hinzufügen wollen.");
 
-        System.out.println("Geben sie eine Aktion an:");
-        System.out.println("[1] Produkt zum Warenkorb hinzufügen");
-        System.out.println("[stop] Produkt Liste verlassen");
+        System.out.println(String.join(" ", mainService.getProductManager().getProducts().keySet().stream()
+                .map(integer -> "" + integer)
+                .toArray(String[]::new)));
 
+        Integer prodictId = getScanner().nextInteger("Bitte geben sie eine Gütlige Produkt Id ein.",
+                mainService.getProductManager().getProducts().keySet().stream()
+                .map(integer -> "" + integer)
+                .toArray(String[]::new));
 
-        String input = getScanner().next("Bitte gebe eine Valide Aktion ein.", "1");
+        System.out.println("Geben Sie die gewünschte Anzahl an:");
 
-        if (input == null) {
-            System.out.println("Produktliste verlassen.");
-            return;
-        }
-
-        if (input.equalsIgnoreCase("1")) {
-            System.out.println("Geben sie die Id des Objektes an das sie zum Warenkorb hinzufügen wollen.");
-
-            System.out.println(String.join(" ", mainService.getProductManager().getProducts().keySet().stream()
-                    .map(integer -> "" + integer)
-                    .toArray(String[]::new)));
-
-            Integer prodictId = getScanner().nextInteger("Bitte geben sie eine Gütlige Produkt Id ein.",
-                    mainService.getProductManager().getProducts().keySet().stream()
-                    .map(integer -> "" + integer)
-                    .toArray(String[]::new));
-
-            System.out.println("Geben Sie die gewünschte Anzahl an:");
-
-            Integer amount = getScanner().nextInteger("Bitte geben sie eine Valide Zahl grösser als 0 an.", integer -> integer > 0);
+        Integer amount = getScanner().nextInteger("Bitte geben sie eine Valide Zahl grösser als 0 an.", integer -> integer > 0);
 
 
-            Product product = mainService.getProductManager().getProducts().get(prodictId);
-            mainService.getShoppingCartService().getCart().addItem(amount, product);
+        Product product = mainService.getProductManager().getProducts().get(prodictId);
+        mainService.getShoppingCartService().getCart().addItem(amount, product);
 
-            System.out.println(product.getName() + " wurde " + amount + " mal hinzugefügt.");
-        }else {
-            System.out.println("invalid user input!");
-        }
+        System.out.println(product.getName() + " wurde " + amount + " mal hinzugefügt.");
+
     }
 }
